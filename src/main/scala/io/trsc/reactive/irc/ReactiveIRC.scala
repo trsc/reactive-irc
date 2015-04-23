2657a27a341ec2c4193a5cf5556804d0dea453b5
Jonas Adler <jns.adlr@gmail.com>
2015-04-23 22:15:22 +0200

    send commands from stdin

--- src/main/scala/io/trsc/reactive/irc/ReactiveIRC.scala
+++ src/main/scala/io/trsc/reactive/irc/ReactiveIRC.scala
@@ -2,12 +2,18 @@ package io.trsc.reactive.irc
 
 import java.net.InetSocketAddress
 
-import akka.stream.scaladsl.StreamTcp
+import akka.actor.ActorSystem
+import akka.stream.ActorFlowMaterializer
+import akka.stream.scaladsl.{BidiFlow, Source, StreamTcp}
+import akka.util.ByteString
 import com.typesafe.config.ConfigFactory
 
 object ReactiveIRC extends App {
 
   val conf = ConfigFactory.load()
+
+  implicit val system = ActorSystem("reactive-irc")
+  implicit val materializer = ActorFlowMaterializer()
 
   val network = new InetSocketAddress(
     conf.getString("reactive-irc.network"),
@@ -15,5 +21,14 @@ object ReactiveIRC extends App {
   )
 
   val connection = StreamTcp().outgoingConnection(network)
+
+  val convertToByteString = (s: String) => ByteString(s)
+  val convertToString = (b: ByteString) => b.utf8String
+
+  val codec = BidiFlow(convertToByteString, convertToString).join(connection)
+
+  Source(() => scala.io.Source.stdin.getLines()).map(_ + "\r\n").via(codec).runForeach { s =>
+    println(s"response: $s")
+  }
 
 }
