cb69f4469899318d8b47bd15145bdb46e7959d91
Jonas Adler <jns.adlr@gmail.com>
2015-04-30 17:36:51 +0200

    get started on designing the api // start wikipedia example

---
+++ example/io/trsc/reactive/irc/example/WikipediaUpdates.scala
@@ -0,0 +1,14 @@
+package io.trsc.reactive.irc.example
+
+import akka.actor.ActorSystem
+import akka.stream.ActorFlowMaterializer
+import io.trsc.reactive.irc.ReactiveIRC
+
+object WikipediaUpdates extends App {
+
+  implicit val system = ActorSystem("wikipedia-update-listener")
+  implicit val materializer = ActorFlowMaterializer()
+
+  ReactiveIRC.listen("irc.wikimedia.org", 6667, "reactive-example", Nil).runForeach(println)
+
+}
--- src/main/scala/io/trsc/reactive/irc/ReactiveIRC.scala
+++ src/main/scala/io/trsc/reactive/irc/ReactiveIRC.scala
@@ -3,64 +3,43 @@ package io.trsc.reactive.irc
 import java.net.InetSocketAddress
 
 import akka.actor.ActorSystem
-import akka.stream.ActorFlowMaterializer
 import akka.stream.scaladsl._
 import akka.util.ByteString
 import io.trsc.reactive.irc.flows.UnzipIrcMessages
-import io.trsc.reactive.irc.protocol.{IrcNormalizeStage, IrcFrameStage, IrcMessage, IrcMessageStage}
+import io.trsc.reactive.irc.protocol.{IrcFrameStage, IrcMessage, IrcMessageStage, IrcNormalizeStage}
 
-/**
- * Implementation
- *
- * Use FlexiRoute to split server messages and channel messages
- *
- * Server messages that need a reaction (e.g. user registered should trigger channel join, PING should trigger PONG)
- * will be designed with a cyclic graph
- *
- * the channel messages flow is the one that will be returned by the API
- *
- * the goal is something like this:
- *
- * ReactiveIRC.join("irc.freenode.net", 6666, "akka" :: "elixir-lang" :: Nil): Source[IrcMessage, Unit]
- *
- */
-object ReactiveIRC extends App {
+object ReactiveIRC {
 
-  implicit val system = ActorSystem("reactive-irc")
-  implicit val materializer = ActorFlowMaterializer()
+  def listen(network: String, port: Int, nick: String, /* unused*/ channels: Seq[String])(implicit s: ActorSystem): Source[IrcMessage, Unit] = {
+    Source() { implicit builder =>
+      import FlowGraph.Implicits._
 
-  // val registeringSource = Source("PASS foobar" :: "NICK reactive-tester" :: "USER guest 0 * :Reactive Tester" :: "JOIN #akka" :: Nil)
+      val connection = Tcp().outgoingConnection(new InetSocketAddress(network, port))
+      val registeringSource = Source(s"NICK $nick" :: s"USER guest 0 * :${nick.capitalize}" :: Nil)
+      // TODO proper logging
+      val log = Flow[ByteString].map(s => {println(s"sending: ${s.utf8String}"); s})
 
-  val ircMessageSource = Source() { implicit builder =>
-    import FlowGraph.Implicits._
+      val convertToByteString = Flow[String]
+        .map(_ + "\r\n")
+        .map(ByteString.apply)
 
-    val connection = Tcp().outgoingConnection(new InetSocketAddress("irc.wikimedia.org", 6667))
-    val registeringSource = Source("NICK reactive-tester" :: "USER guest 0 * :Reactive Tester" :: Nil)
-    // TODO remove lame logging
-    val log = Flow[ByteString].map(s => {println(s"sending: ${s.utf8String}"); s})
+      val decodeIrcMessages = Flow[ByteString]
+        .transform(() => new IrcFrameStage)
+        .transform(() => new IrcNormalizeStage)
+        .transform(() => new IrcMessageStage)
 
-    val convertToByteString = Flow[String]
-                                .map(_ + "\r\n")
-                                .map(ByteString.apply)
+      // TODO properly implement the Request Response Protocol
+      val testFlow = Flow[IrcMessage].filter(_.command == "001").map(_ => "JOIN #en.wikipedia\r\n").map(ByteString.apply)
 
-    val decodeIrcMessages = Flow[ByteString]
-                              .transform(() => new IrcFrameStage)
-                              .transform(() => new IrcNormalizeStage)
-                              .transform(() => new IrcMessageStage)
+      val merge = builder.add(MergePreferred[ByteString](1))
 
-    // TODO properly implement the Request Response Protocol
-    val testFlow = Flow[IrcMessage].filter(_.command == "376").map(_ => "JOIN #en.wikipedia\r\n").map(ByteString.apply)
+      val splitMessages = builder.add(new UnzipIrcMessages)
 
-    val merge = builder.add(MergePreferred[ByteString](1))
+      registeringSource ~> convertToByteString ~> merge ~> log ~> connection ~> decodeIrcMessages ~> splitMessages.in
+      merge.preferred    <~    testFlow    <~    splitMessages.systemMessages
 
-    val splitMessages = builder.add(new UnzipIrcMessages)
-
-    val f = registeringSource ~> convertToByteString ~> merge ~> log ~> connection ~> decodeIrcMessages ~> splitMessages.in
-                                                        merge.preferred    <~     testFlow    <~    splitMessages.systemMessages
-
-    splitMessages.channelMessages
+      splitMessages.channelMessages
+    }
   }
-
-  ircMessageSource.runForeach(println)
 
 }
